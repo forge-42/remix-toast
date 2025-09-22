25ca876e54a03f1469a506ef8c61151d513a63fa
Andrei Bobkov <slider7259@gmail.com>
2025-09-22 21:15:18 +0200

    chore(repo): format

--- src/schema.ts
+++ src/schema.ts
@@ -1,20 +1,18 @@
 import { z } from "zod";
 
-export const toastMessageSchema = z
-  .looseObject({
-    message: z.string(),
-    description: z.string().optional(),
-    duration: z.number().int().nonnegative().optional(),
-    type: z.custom<"info" | "success" | "error" | "warning">(),
-  })
+export const toastMessageSchema = z.looseObject({
+  message: z.string(),
+  description: z.string().optional(),
+  duration: z.number().int().nonnegative().optional(),
+  type: z.custom<"info" | "success" | "error" | "warning">(),
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const toastMessageWithoutTypeSchema = z
-  .looseObject({
-    message: z.string(),
-    description: z.string().optional(),
-    duration: z.number().int().nonnegative().optional(),
-  });
+const toastMessageWithoutTypeSchema = z.looseObject({
+  message: z.string(),
+  description: z.string().optional(),
+  duration: z.number().int().nonnegative().optional(),
+});
 
 export const flashSessionValuesSchema = z.object({
   toast: toastMessageSchema.optional(),
